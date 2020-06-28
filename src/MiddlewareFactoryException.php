3d1bd26905f3a5455c970032bafa6b15cc139f85
Andrey <alestro@bk.ru>
2020-06-28 13:20:55 +0200

    Update and rename UnresolvableMiddlewareException.php to MiddlewareFactoryException.php

---
+++ src/MiddlewareFactoryException.php
@@ -0,0 +1,70 @@
+<?php
+
+
+namespace Lobster\MiddlewareFactory;
+
+
+use Lobster\Type;
+
+
+/**
+ * Class MiddlewareFactoryException
+ * @package Lobster\MiddlewareFactory
+ */
+class MiddlewareFactoryException extends \RuntimeException 
+{
+    /**
+     * @param $middleware
+     * @throws static
+     */
+    public function throw() : void
+    {
+        throw this;
+    }
+    
+    /**
+     * @param $any
+     * @return static
+     */
+    public static notCreatable($any) : self
+    {
+        $type = Type::gettype($any, Type::objectAsClass);
+
+        if ($type == Type::callable)
+        {
+            $type = static::getTypeForCallable($type);
+        }
+        
+        return new static('Cannot create middleware for this type: ' . $type);
+    }
+    
+    /**
+     * @param callable $any
+     * @param string $returnType
+     * @return static
+     */
+    public static invalidReturnType(callable $any, string $returnType) : void 
+    {
+        return new static('Callable: ' . $type . 'should return an Psr\Http\Message\ResponseInterface. Returned '. $returnType);
+    }
+    
+    private static function geTypeForCallable(callable $type) : string
+    {
+        if(is_object($any))
+        {
+            return get_class($any);
+        }
+            
+        if(is_array($any))
+        {
+            return new \ReflectionMethod($any[0], $any[1])->getName();
+        }
+        
+        if(str_pos($any, '::') !== false)
+        {
+            return new \ReflectionMethod($any)->getName();
+        }
+        
+        return new \ReflectionFunction($any)->getName();
+    }
+}
