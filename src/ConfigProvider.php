227637e29e363680f1929bb53c6bb7c77d020871
Andrey <alestro@bk.ru>
2020-07-12 23:54:41 +0200

    Create ConfigProvider.php

---
+++ src/ConfigProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace Bermuda\MiddlewareFactory;
+
+
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Bermuda\Pipeline\PipelineFactoryInterface;
+
+
+
+final class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return ['dependencies' => 
+            ['factories' => [MiddlewareFactoryInterface::class => function(ContainerInterface $c)
+            {
+                return new MiddlewareFactory($c, $c->get(ResponseFactoryInterface::class), $c->get(MiddlewareFactoryInterface::class));
+            }]
+        ]];
+    }
+}
