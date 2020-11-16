3e03ebc0125782c659ae0978c7376df08891a087
Andrey <inbox.shelamkoff@gmail.com>
2020-11-16 14:16:36 +0300

    Update MiddlewareFactory.php

--- src/MiddlewareFactory.php
+++ src/MiddlewareFactory.php
@@ -27,7 +27,7 @@ final class MiddlewareFactory implements MiddlewareFactoryInterface
     private ContainerInterface $container;
     private ResponseFactoryInterface $responseFactory;
     private PipelineFactoryInterface $pipelineFactory;
-    
+
     public const separator = '@';
 
     public function __construct(
@@ -59,29 +59,29 @@ final class MiddlewareFactory implements MiddlewareFactoryInterface
             }
 
             if (str_contains($any, self::separator) !== false)
-            {   
-               list($service, $method) = explode(self::separator, $any, 2);
+            {
+                list($service, $method) = explode(self::separator, $any, 2);
 
-               if ($this->container->has($service) && method_exists($service, $method))
-               {
+                if ($this->container->has($service) && method_exists($service, $method))
+                {
                     $any = [$this->service($service), $method];
                     goto callback;
-               }
+                }
             }
-            
+
             if (is_callable($any))
             {
                 goto str_callback;
             }
-            
+
             goto end;
         }
-           
+
         if ($any instanceof MiddlewareInterface)
         {
             return $any;
         }
-        
+
         if ($any instanceof RequestHandlerInterface)
         {
             return new Decorator\RequestHandlerDecorator($any);
@@ -94,7 +94,7 @@ final class MiddlewareFactory implements MiddlewareFactoryInterface
                 $method = (new \ReflectionObject($any))
                     ->getMethod('__invoke');
             }
-            
+
             elseif (is_array($any))
             {
                 callback:
@@ -107,94 +107,93 @@ final class MiddlewareFactory implements MiddlewareFactoryInterface
                 {
                     $method = new \ReflectionMethod($any);
                 }
-                
+
                 else
                 {
                     str_callback:
                     $method = new \ReflectionFunction($any);
                 }
             }
-            
+
             $returnType = $method->getReturnType();
-            
-            if (!$returnType instanceof \ReflectionNamedType || ($returnType->getName() != 'Psr\Http\Message\ResponseInterface' && 
-                                                                 $returnType->getName() != MiddlewareInterface::class))
+
+            if (!$returnType instanceof \ReflectionNamedType || ($returnType->getName() != 'Psr\Http\Message\ResponseInterface' &&
+                    $returnType->getName() != MiddlewareInterface::class))
             {
                 MiddlewareFactoryException::invalidReturnType($any, 'void')->throw();
             }
-            
+
             if ($returnType->getName() == MiddlewareInterface::class)
             {
                 try
                 {
                     return $any($this->container);
                 }
-                
+
                 catch (\Throwable $e)
                 {
                     MiddlewareFactoryException::fromPrevios($e, $any)->throw();
                 }
             }
-            
+
             if (($count = count($parameters = $method->getParameters())) == 0)
             {
                 return new Decorator\CallbackDecorator($any);
             }
-
-            if ($count == 1)
+            
+            if ($this->checkType($parameters[0], ServerRequestInterface::class))
             {
-                if ($this->checkType($parameters[0], ServerRequestInterface::class))
+                if ($count == 1)
                 {
                     return new Decorator\CallbackDecorator($any);
                 }
-            }
 
-            if ($count == 2)
-            {
-                if ($this->checkType($parameters[0], ServerRequestInterface::class) &&
-                    $this->checkType($parameters[1], RequestHandlerInterface::class))
+                if ($count == 2)
                 {
-                     return new Decorator\CallbackDecorator($any);
+                    if ($this->checkType($parameters[1], RequestHandlerInterface::class))
+                    {
+                        return new Decorator\CallbackDecorator($any);
+                    }
+
+                    if ($parameters[1]->isCallable())
+                    {
+                        return new Decorator\SinglePassDecorator($any);
+                    }
                 }
 
-                if ($this->checkType($parameters[0], ServerRequestInterface::class)
-                    && $parameters[1]->isCallable())
+                if ($count === 3)
                 {
-                    return new Decorator\SinglePassDecorator($any);
+                    if ($this->checkType($parameters[1], ResponseInterface::class)
+                        && $parameters[2]->isCallable())
+                    {
+                        return new Decorator\DoublePassDecorator($any, $this->responseFactory);
+                    }
                 }
             }
-
-            if ($count === 3)
-            {
-                if ($this->checkType($parameters[0], ServerRequestInterface::class) &&
-                    $this->checkType($parameters[0], ResponseInterface::class)
-                    && $parameters[2]->isCallable())
-                {
-                    return new Decorator\DoublePassDecorator($any, $this->responseFactory);
-                }
-            }
+            
+            return new Decorator\ArgumentDecorator($parameters);
         }
-        
+
         if (is_iterable($any))
         {
             $pipeline = $this->pipelineFactory->make();
-            
+
             foreach ($any as $item)
             {
                 $pipeline->pipe($this->make($item));
             }
-            
+
             return $pipeline;
         }
 
         end:
         MiddlewareFactoryException::notCreatable($any)->throw();
     }
-    
+
     /**
      * @inheritDoc
      */
-    public function __invoke($any): MiddlewareInterface 
+    public function __invoke($any): MiddlewareInterface
     {
         return $this->make($any);
     }
