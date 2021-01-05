9cd68a0e1d2accbe6d1c27795f8d84181614a691
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-01-05 19:46:16 +0200

    Update CallbackDecorator.php

--- src/decorator/CallbackDecorator.php
+++ src/decorator/CallbackDecorator.php
@@ -1,9 +1,9 @@
 <?php
 
-
 namespace Bermuda\MiddlewareFactory\Decorator;
 
 
+use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -33,5 +33,47 @@ class CallbackDecorator implements MiddlewareInterface
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         return ($this->callback)($request, $handler);
+    }
+
+    /**
+     * @return static
+     */
+    public static function decorateEmptyArgsCallable(callable $callable): self
+    {
+        return new class($callable) extends CallbackDecorator
+        {
+            /**
+             * @inheritDoc
+             */
+            public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+            {
+                return ($this->callback)();
+            }
+        };
+    }
+
+    /**
+     * @return static
+     */
+    public static function decorateContainerArgCallable(callable $callable, ContainerInterface $container): self
+    {
+        return new class($callable, $container) extends CallbackDecorator
+        {
+            private ContainerInterface $container;
+            
+            public function __construct(callable $callback, ContainerInterface $container)
+            {
+                $this->container = $container;
+                parent::__construct($callback);
+            }
+
+            /**
+             * @inheritDoc
+             */
+            public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+            {
+                return ($this->callback)($this->container);
+            }
+        };
     }
 }
