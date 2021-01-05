59d915ed19bec71667900b1e6f09befb676c1f65
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-01-05 21:38:23 +0200

    Update MiddlewareFactoryFactory.php

--- src/MiddlewareFactoryFactory.php
+++ src/MiddlewareFactoryFactory.php
@@ -5,9 +5,13 @@ namespace Bermuda\MiddlewareFactory;
 
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
-use Bermuda\MiddlewareFactory\PipelineFactoryInterface;
+use Bermuda\Pipeline\PipelineFactoryInterface;
 
 
+/**
+ * Class MiddlewareFactoryFactory
+ * @package Bermuda\MiddlewareFactory
+ */
 final class MiddlewareFactoryFactory
 {
     public function __invoke(ContainerInterface $container): MiddlewareFactory
