5c018de2ea9dc632e439fdd2e43f15d4b8559352
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-03-25 23:54:50 +0300

    Update CallbackDecorator.php

--- src/decorator/CallbackDecorator.php
+++ src/decorator/CallbackDecorator.php
@@ -2,14 +2,12 @@
 
 namespace Bermuda\MiddlewareFactory\Decorator;
 
-
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
-
 
 /**
  * Class CallbackDecorator
