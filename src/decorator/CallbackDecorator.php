ca92e53c943199b3dd302f606605d4ab9fd0cbcd
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-10 03:04:18 +0200

    Update CallbackDecorator.php

--- src/decorator/CallbackDecorator.php
+++ src/decorator/CallbackDecorator.php
@@ -4,50 +4,23 @@ namespace Bermuda\MiddlewareFactory\Decorator;
 
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Psr\Http\Message\ResponseFactoryInterface;
 
-/**
- * Class CallbackDecorator
- * @package Bermuda\MiddlewareFactory\Decorator
- */
 class CallbackDecorator implements MiddlewareInterface
 {
     /**
      * @var callable
      */
     protected $callback;
-    
-    public function __construct(callable $callback)
-    {
-        $this->callback = $callback;
-    }
-    
-    /**
-     * @return callable
-     */
-    public function getCallback(): callable
-    {
-        return $this->callback;
-    }
-    
-    /**
-     * @inheritDoc
-     */
-    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
-    {
-        return ($this->callback)($request, $handler);
-    }
 
     /**
      * @return static
      */
     public static function decorateEmptyArgsCallable(callable $callable): self
     {
-        return new class($callable) extends CallbackDecorator
-        {
+        return new class($callable) extends CallbackDecorator {
             /**
              * @inheritDoc
              */
@@ -63,10 +36,9 @@ class CallbackDecorator implements MiddlewareInterface
      */
     public static function decorateContainerArgCallable(callable $callable, ContainerInterface $container): self
     {
-        return new class($callable, $container) extends CallbackDecorator
-        {
+        return new class($callable, $container) extends CallbackDecorator {
             private ContainerInterface $container;
-            
+
             public function __construct(callable $callback, ContainerInterface $container)
             {
                 $this->container = $container;
@@ -81,5 +53,26 @@ class CallbackDecorator implements MiddlewareInterface
                 return ($this->callback)($this->container);
             }
         };
+    }
+
+    public function __construct(callable $callback)
+    {
+        $this->callback = $callback;
+    }
+
+    /**
+     * @return callable
+     */
+    public function getCallback(): callable
+    {
+        return $this->callback;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        return ($this->callback)($request, $handler);
     }
 }
