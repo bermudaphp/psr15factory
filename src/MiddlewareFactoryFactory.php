4d41452fcd99bb6613fd7f57ac4206680a345ee1
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-10 03:06:37 +0200

    Update MiddlewareFactoryFactory.php

--- src/MiddlewareFactoryFactory.php
+++ src/MiddlewareFactoryFactory.php
@@ -2,36 +2,25 @@
 
 namespace Bermuda\MiddlewareFactory;
 
-
+use Bermuda\Pipeline\PipelineFactoryInterface;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
-use Bermuda\Pipeline\PipelineFactoryInterface;
 
-
-/**
- * Class MiddlewareFactoryFactory
- * @package Bermuda\MiddlewareFactory
- */
 final class MiddlewareFactoryFactory
 {
-    public function __invoke(ContainerInterface $container): MiddlewareFactory
-    {
-        return new MiddlewareFactory($container, $container->get(ResponseFactoryInterface::class), $container->get(PipelineFactoryInterface::class));
-    }
-    
-     /**
-      * @param MiddlewareFactoryInterface[] $factories
-      * @return AggregateMiddlewareFactory
-      */
     public static function makeOf(iterable $factories = []): AggregateMiddlewareFactory
     {
         $aggregate = new AggregateMiddlewareFactory();
-        
-        foreach($factories as $factory)
-        {
+
+        foreach ($factories as $factory) {
             $aggregate->addFactory($factory);
         }
-        
+
         return $aggregate;
+    }
+
+    public function __invoke(ContainerInterface $container): MiddlewareFactory
+    {
+        return new MiddlewareFactory($container, $container->get(ResponseFactoryInterface::class), $container->get(PipelineFactoryInterface::class));
     }
 }
