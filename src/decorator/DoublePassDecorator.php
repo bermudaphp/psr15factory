9326e811c17797d5e47a7df1fc408ab1a9e710b8
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-10 03:03:40 +0200

    Update DoublePassDecorator.php

--- src/decorator/DoublePassDecorator.php
+++ src/decorator/DoublePassDecorator.php
@@ -2,31 +2,26 @@
 
 namespace Bermuda\MiddlewareFactory\Decorator;
 
+use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Psr\Http\Message\ResponseFactoryInterface;
 
-/**
- * Class DoublePassDecorator
- * @package Bermuda\MiddlewareFactory\Decorator
- */
 final class DoublePassDecorator extends CallbackDecorator
 {
-  private ResponseFactoryInterface $factory;
-  
-  public function __construct(callable $callback, ResponseFactoryInterface $factory)
-  {
-      $this->factory = $factory;
-      parent::__construct($callback);
-  }
+    private ResponseFactoryInterface $factory;
 
-  /**
-   * @inheritDoc
-   */
-  public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
-  {
-      return ($this->middleware)($request, $this->factory->createResponse(), static fn(ServerRequestInterface $request): ResponseInterface => $handler->handle($request);
-  }
+    public function __construct(callable $callback, ResponseFactoryInterface $factory)
+    {
+        $this->factory = $factory;
+        parent::__construct($callback);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        return ($this->callback)($request, $this->factory->createResponse(), static fn(ServerRequestInterface $request): ResponseInterface => $handler->handle($request));
+    }
 }
