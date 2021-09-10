c6c72ce7d80586ec8c1740b2ee1113faff64a6f3
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-10 03:00:01 +0200

    Update SinglePassDecorator.php

--- src/decorator/SinglePassDecorator.php
+++ src/decorator/SinglePassDecorator.php
@@ -3,15 +3,9 @@
 namespace Bermuda\MiddlewareFactory\Decorator;
 
 use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Psr\Http\Message\ResponseFactoryInterface;
 
-/**
- * Class SinglePassDecorator
- * @package Bermuda\MiddlewareFactory\Decorator
- */
 final class SinglePassDecorator extends CallbackDecorator
 {
     /**
@@ -19,8 +13,7 @@ final class SinglePassDecorator extends CallbackDecorator
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        return ($this->callback)($request, static function (ServerRequestInterface $request) use ($handler): ResponseInterface
-        {
+        return ($this->callback)($request, static function (ServerRequestInterface $request) use ($handler): ResponseInterface {
             return $handler->handle($request);
         });
     }
