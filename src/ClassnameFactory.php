077ce43a98a12ae01f3090e0553a791b47fe88fb
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-04-07 12:46:38 +0200

    Create ClassnameFactory.php

---
+++ src/ClassnameFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Bermuda\MiddlewareFactory;
+
+use Bermuda\MiddlewareFactory\Decorator\RequestHandlerDecorator;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+final class ClassnameFactory implements MiddlewareFactoryInterface
+{
+    public function __construct(
+        private readonly ContainerInterface $container
+    ) {
+    }
+
+    public function make($any): MiddlewareInterface
+    {
+        try {
+            $middleware = $container->get($any);
+            if ($middleware instanceof MiddlewareInterface) return $middleware;
+            if ($middleware instanceof RequestHandlerInterface) return new RequestHandlerDecorator($middleware);
+            throw UnresolvableMiddlewareException::notCreatable($any);
+        } catch (ContainerExceptionInterface|NotFoundExceptionInterface $e) {
+            throw UnresolvableMiddlewareException::fromPrevious($e, $any);
+        }
+    }
+}
