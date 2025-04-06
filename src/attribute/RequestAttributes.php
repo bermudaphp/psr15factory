c73ac1f1fc29b24eee8a4c61067605c34e4d802b
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-04-06 17:32:27 +0200

    Update RequestAttributes.php

--- src/attribute/RequestAttributes.php
+++ src/attribute/RequestAttributes.php
@@ -7,20 +7,20 @@ use Psr\Http\Message\ServerRequestInterface;
 #[\Attribute(\Attribute::TARGET_PARAMETER)] class RequestAttributes
 {
     public function __construct(
-        public readonly string $name,
+        public readonly ?string $name=null,
     ) {
     }
 
     public function getParameter(ServerRequestInterface $request, \ReflectionParameter $parameter): array
     {
-        if (!isset($request->getAttributes()[$this->name])) {
+        if (!isset($request->getAttributes()[$name = $this->name ?? $parameter->getName()])) {
             if ($parameter->isDefaultValueAvailable()) {
                 return [$parameter->getName(), $parameter->getDefaultValue()];
             }
 
-            throw new \RuntimeException('Missed request attribute "' . $this->name . '"');
+            throw new \RuntimeException('Missed request attribute "' . $name . '"');
         }
 
-        return [$parameter->getName(), $request->getAttributes()[$this->name]];
+        return [$parameter->getName(), $request->getAttributes()[$name]];
     }
 }
