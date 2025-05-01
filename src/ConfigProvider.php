b8b8b57690c630529086b0763f10cebfdfe99bbb
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-05-01 20:18:07 +0200

    Update ConfigProvider.php

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -2,8 +2,15 @@
 
 namespace Bermuda\MiddlewareFactory;
 
+use Bermuda\MiddlewareFactory\Resolver\RequestAttributeResolver;
+use Bermuda\MiddlewareFactory\Resolver\RequestMapperResolver;
 use Bermuda\MiddlewareFactory\Strategy\CallableStrategy;
 use Bermuda\MiddlewareFactory\Strategy\ClassNameStrategy;
+use Bermuda\MiddlewareFactory\Strategy\MiddlewarePipelineStrategy;
+use Bermuda\ParameterResolver\ParameterResolver;
+use Bermuda\ParameterResolver\ParameterResolverInterface;
+use Bermuda\DI\FactoryInterface;
+use Psr\Container\ContainerInterface;
 
 class ConfigProvider extends \Bermuda\Config\ConfigProvider
 {
@@ -15,6 +22,9 @@ class ConfigProvider extends \Bermuda\Config\ConfigProvider
             MiddlewareFactory::class => [MiddlewareFactory::class, 'createFromContainer'],
             CallableStrategy::class => [CallableStrategy::class, 'createFromContainer'],
             ClassNameStrategy::class => [ClassNameStrategy::class, 'createFromContainer'],
+            MiddlewarePipelineStrategy::class => [MiddlewarePipelineStrategy::class, 'createFromContainer'],
+            RequestMapperResolver::class => [self::class, 'createRequestMapperResolver'],
+            ParameterResolver::class => [self::class, 'createParameterResolver']
         ];
     }
 
@@ -23,8 +33,25 @@ class ConfigProvider extends \Bermuda\Config\ConfigProvider
         return [MiddlewareFactoryInterface::class => MiddlewareFactory::class];
     }
 
-    protected function getProviders(): array
+    protected function getInvokables(): array
     {
-        return [\Bermuda\ParameterResolver\ConfigProvider::class];
+        return [RequestAttributeResolver::class];
+    }
+    
+    private function createRequestMapperResolver(ContainerInterface $container): RequestMapperResolver
+    {
+        return new RequestMapperResolver($container->get(FactoryInterface::class));
+    }
+
+    private function createParameterResolver(ContainerInterface $container): ParameterResolver
+    {
+        if ($container->has(ParameterResolver::class)) {
+            $resolver = $container->get(ParameterResolver::class);
+        } else $resolver = ParameterResolver::createDefaults($container);
+
+        $resolver->addResolver($container->get(RequestAttributeResolver::class), true);
+        $resolver->addResolver($container->get(RequestMapperResolver::class), true);
+        
+        return $resolver;
     }
 }
